932672a1c99a621f8878a06910b6fb88ea3a404f
WajidKagzi <wjkagzi@gmail.com>
2016-04-04 23:57:50 -0400

    Adding some custom animation

--- NumbersPlayground.playground/Contents.swift
+++ NumbersPlayground.playground/Contents.swift
@@ -19,3 +19,19 @@ let myDouble = Double(myInt)
 var str = "Hello playground!"
 
 str += "!!!+😂"
+
+import Advance
+
+class MyClass {
+    
+    let center: Animatable<CGPoint>
+    
+    init() {
+        ...
+            center.changed.observe { (value) in
+                // Do something every time the center value changes
+        }
+    }
+}
+
+let foo = MyClass()
